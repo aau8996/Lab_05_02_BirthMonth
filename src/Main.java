4d8aee3dcb110710898e91fa05799adc34cb443e
aau8996 <auad@mail.uc.edu>
2023-02-12 12:12:15 -0500

    Final Code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,84 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Variables and simulated input
+        int birthMonth = 13;
+        // Cascaded if then else; if birthMonth == 1 then
+        if(birthMonth == 1)
+        {
+            // output "Your birth month is:" + birthMonth
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        // else if birthMonth == 2 then
+        else if(birthMonth == 2)
+        {
+            // output "Your birth month is:" + birthMonth
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        // else if birthMonth == 3 then
+        else if(birthMonth == 3)
+        {
+            // output "Your birth month is:" + birthMonth
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        // else if birthMonth == 4 then
+        else if(birthMonth == 4)
+        {
+            // output "Your birth month is:" + birthMonth
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        // else if birthMonth == 5 then
+        else if(birthMonth == 5)
+        {
+            // output "Your birth month is:" + birthMonth
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        // else if birthMonth == 6 then
+        else if(birthMonth == 6)
+        {
+            // output "Your birth month is:" + birthMonth
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        // else if birthMonth == 7 then
+        else if(birthMonth == 7)
+        {
+            // output "Your birth month is:" + birthMonth
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        // else if birthMonth == 8 then
+        else if(birthMonth == 8)
+        {
+            // output "Your birth month is:" + birthMonth
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        // else if birthMonth == 9 then
+        else if(birthMonth == 9)
+        {
+            // output "Your birth month is:" + birthMonth
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        // else if birthMonth == 10 then
+        else if(birthMonth == 10)
+        {
+            // output "Your birth month is:" + birthMonth
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        // else if birthMonth == 11 then
+        else if(birthMonth == 11)
+        {
+            // output "Your birth month is:" + birthMonth
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        // else if birthMonth == 12 then
+        else if(birthMonth == 12)
+        {
+            // output "Your birth month is:" + birthMonth
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        // else for invalid input
+        else
+        {
+            // output for invalid input
+            System.out.println("You entered an incorrect month value: " + birthMonth);
+        }
     }
 }
